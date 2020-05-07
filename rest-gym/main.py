1449c0e69dee1df14a73903ae10fd15f3fc3fae2
benedikt asshauer <benediktasshauer@gmx.de>
2020-05-07 13:29:53 +0200

    updated main

--- rest-gym/main.py
+++ rest-gym/main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 @author: Benedikt
 """
@@ -11,42 +10,55 @@ from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 from stable_baselines import A2C
 from stable_baselines import DQN
+import pp_helpers
 
-if 'Rest_Minimal-v0' in gym.envs.registry.env_specs:
-    del gym.envs.registry.env_specs['Rest_Minimal-v0']
-    print("unregistered {} env".format('Rest_Minimal-v0'))
+if 'Restoration_Env-v0' in gym.envs.registry.env_specs:
+    del gym.envs.registry.env_specs['Restoration_Env-v0']
+    print("unregistered {} env".format('Restoration_Env-v0'))
 
 import gym_rest
 
-env = gym.make('Rest_Minimal-v0')
+env = gym.make('Restoration_Env-v0')
+env_dummy = gym.make('Restoration_Env-v0')
+
 # Vectorized environments allow to easily multiprocess training
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
 env.reset()
 
+### set model
+
 #model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='ppo2_rest_tensorboard/')
 #model = PPO2(MlpPolicy, env, verbose=1)
-model = A2C(MlpPolicy, env, verbose=1, tensorboard_log='a2c_rest_tensorboard/')
+#model = A2C(MlpPolicy, env, verbose=1, tensorboard_log='a2c_rest_tensorboard/')
 #model = DQN(MlpPolicy, env, verbose=1, tensorboard_log='dqn_rest_tensorboard')
+#note: DQN training requires a different set of MlpPolicy 
+
+#### or load a pretrained model
+
+model = A2C.load("trained_agents/A2C_14node_200000steps_newrewards", env=env)
+
+### Train the agent
+
+#t1 = time.time()
+#model.learn(total_timesteps=250000)
+#t2 = time.time()
+#print("learning took {} seconds".format(t2-t1))
+
+### enjoy the trained agent
 
 
 
-# Train the agent
+### save model
 
-#use tensorboard with the following line in anaconda prompt: 
-# Benedikt\Documents\Uni\MasterThesis\Code\project05_gym_continuous\rest-gym
-# tensorboard --logdir C:\Users\Benedikt\Documents\Uni\MasterThesis\Code\project05_gym_continuous/rest-gym/a2c_rest_tensorboard/
+#model.save("A2C_14node_200000steps")
 
-t1 = time.time()
-model.learn(total_timesteps=1000000)
-t2 = time.time()
-print("learning took {} seconds".format(t2-t1))
-
-#model = A2C.load("a2c_4node_100000steps")
+### plot a restoration sequence 
 
 #pp_helpers.plot_sequence(env, model)
 
-#action_list_func = env.env_method("_get_action_list")[0]
+# get an action list of the env to understand the different actions
 
-# save model
-#model.save("A2C_39node_1000000steps")
+action_list_func = env.env_method("get_action_df")
+
+
